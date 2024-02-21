7aab70018a438afa7a431ed4d6c3d8720417c38b
lmurillo248 <lmurillo248@gmail.com>
2024-02-21 12:45:41 -0700

    Modifique el main para mostr

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Mi primer proyecto'),
+      home: const MyHomePage(title: 'Mi primer proyectos'),
     );
   }
 }
